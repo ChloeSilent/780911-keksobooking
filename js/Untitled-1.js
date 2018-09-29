e70a4822aae1c3b65c9273d9747de60feb52c0e9
Olga <hloesilent@gmail.com>
2018-09-29 19:15:42 +0300

    replaced map file with new onw just to try

--- js/Untitled-1.js
+++ js/Untitled-1.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var getRandom = function(min,max){
+    return number = Math.floor(Math.random() * (max - min)) + min;
+  }
+
 var title = ['Большая уютная квартира',
   'Маленькая неуютная квартира',
   'Огромный прекрасный дворец',
@@ -10,32 +14,25 @@ var title = ['Большая уютная квартира',
   'Неуютное бунгало по колено в воде'
 ];
 
-// var address = [
-//   locationX(),
-//   locationY()
-// ];
-
 var locationX = function () {
   var minX = 100;
   var maxX = 1087;
-  var X;
 
-  return X = Math.floor(Math.random() * (maxX - minX)) + minX;
+  return getRandom(minX, maxX);
 }
 
 var locationY = function () {
   var minY = 130;
   var maxY = 630;
-  var Y;
 
-  return Y = Math.floor(Math.random() * (maxY - minY)) + minY;
+  return  getRandom(minY, maxY);
 }
 
 var price = function () {
   var min = 1000;
   var max = 1000000;
-
-  return Math.floor(Math.random() * (max - min)) + min;
+  var Price = getRandom(min, max);
+  return Price;
 }
 
 var typeMatch = ['palace', 'flat', 'house', 'bungalo'];
@@ -59,10 +56,10 @@ var rooms = function () {
   var min = 1;
   var max = 5;
 
-  return Math.floor(Math.random() * (max - min)) + min;
+  return getRandom(min, max);;
 }
 
-var guests = function () {
+var guests = function(){
   var guestNumber = Math.floor(Math.random() * 10);
   while (guestNumber === 0) {
     guestNumber = Math.random();
@@ -76,7 +73,7 @@ var checkout = checkin;
 var getTime1 = function () {
   var min = 1;
   var max = 3;
-  var i = Math.floor(Math.random() * (max - min)) + min;
+  var i = getRandom(min, max);;
   var time = checkin[i - 1];
   return time;
 }
@@ -84,14 +81,14 @@ var getTime1 = function () {
 var getTime2 = function () {
   var min = 1;
   var max = 3;
-  var i = Math.floor(Math.random() * (max - min)) + min;
+  var i = getRandom(min, max);;
   var time = checkout[i - 1];
   return time;
 }
 
 var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
 
-var description = '';
+var description =  '';
 
 var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
@@ -121,6 +118,7 @@ var putPin = function (number) {
   }
 
 }
+
 var body = document.querySelector('body');
 var wherePutCard = document.querySelector('.map__filters-container');
 var templateCard = document.body.querySelector('#card').content.querySelector('.map__card');
@@ -128,36 +126,8 @@ var fragmentCard = document.createDocumentFragment();
 var photos__card = templateCard.querySelector('.popup__photos');
 var features__template = templateCard.querySelector('.popup__features');
 
-
-/*
-
-<ul class="popup__features">
-        <li class="popup__feature popup__feature--wifi"></li>
-        <li class="popup__feature popup__feature--dishwasher"></li>
-        <li class="popup__feature popup__feature--parking"></li>
-        <li class="popup__feature popup__feature--washer"></li>
-        <li class="popup__feature popup__feature--elevator"></li>
-        <li class="popup__feature popup__feature--conditioner"></li>
-      </ul>*/
-
-var getRandom = function (min, max) {
+var getRandom = function(min,max){
   return number = Math.floor(Math.random() * (max - min)) + min;
-}
-
-var getFeatures = function () {
-  var ranNumber = getRandom(1, 7);
-  var featuresUl = templateCard.querySelector('.popup__features');
-  var featuresLi = featuresUl.cloneNode(true);
-  console.log(featuresLi);
-
-  // for(var i = 0; i < ranNumber; i++ ){
-
-
-  //   //var featuresLi = featuresUl.appendChild(featuresLi[i]);
-  // }
-
-
-  return featuresList;
 }
 
 var createCard = function (number) {
@@ -168,16 +138,17 @@ var createCard = function (number) {
     element.querySelector('.popup__type').textContent = getType();
     element.querySelector('.popup__text--capacity').textContent = rooms() + ' комнаты для ' + guests() + ' гостей';
     element.querySelector('.popup__text--time').textContent = 'Заезд после ' + getTime1() + ', выезд до ' + getTime2();
-    element.querySelector('.popup__features').textContent = getFeatures();
 
     element.querySelector('.popup__description').textContent = description;
-    for (var j = 0; j <= photos.length; j++) {
-      photos__card.querySelector('img').src = photos[j];
+    for(var j = 0; j <= photos.length; j++){
+    photos__card.querySelector('img').src = photos[j];
     }
     fragmentCard.appendChild(element);
     wherePutCard.appendChild(fragmentCard);
   }
 
 }
+
 putPin([1, 1, 1, 1, 1, 1, 1, 1]);
-createCard([1, 1, 1, 1, 1, 1, 1]);
+createCard([ 1, 1, 1, 1, 1, 1, 1]);
+
