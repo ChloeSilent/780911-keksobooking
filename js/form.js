7a3dc313c79449baa544440bd1e217a593c07233
Olga <hloesilent@gmail.com>
2018-10-30 23:44:59 +0300

    first attempt to make load func

--- js/form.js
+++ js/form.js
@@ -167,4 +167,19 @@
     submitButtonElement.removeEventListener('mousedown', onSubmitButtonElementClick);
   };
 
+  var mainElement = document.querySelector('main');
+  var fragment = document.createDocumentFragment();
+  var successHandler = function () {
+    var succesMessageClone = document.querySelector('#success').cloneNode(true);
+    fragment.appendChild(succesMessageClone);
+    mainElement.appendChild(fragment);
+  };
+  var errorHandler = function () {
+    var errorMessageClone = document.querySelector('#error').cloneNode(true);
+    fragment.appendChild(errorMessageClone);
+    mainElement.appendChild(fragment);
+  };
+
+  window.backend.load(successHandler, errorHandler);
+
 })();
