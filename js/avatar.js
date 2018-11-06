76cd353cfc847214bde00cdde559c106c20a20cc
Olga <hloesilent@gmail.com>
2018-11-06 03:31:09 +0300

    add avatar module - to upload images - work, drag and drop in process + fixed bug from filter

--- js/avatar.js
+++ js/avatar.js
@@ -5,68 +5,12 @@
 
   window.avatar = {};
 
-  // var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-
-
-  // var fileChooser = document.querySelector('.ad-form__field input[type=file]');
-  // var preview = document.querySelector('.ad-form-header__preview').querySelector('img');
-  // var dropZoneElement = document.querySelector('.ad-form-header__drop-zone');
-
-  // var uploadFile = function () {
-  //   var file = fileChooser.files[0];
-  //   var fileName = file.name.toLowerCase();
-
-  //   var matches = FILE_TYPES.some(function (it) {
-  //     return fileName.endsWith(it);
-  //   });
-
-  //   if (matches) {
-  //     var reader = new FileReader();
-
-  //     reader.addEventListener('load', function () {
-  //       preview.src = reader.result;
-  //     });
-
-  //     if (file) {
-  //       reader.readAsDataURL(file);
-  //     } else {
-  //       preview.src = '';
-  //     }
-  //   }
-  // };
-
-  // fileChooser.addEventListener('change', uploadFile);
-
-  // function dragenter(e) {
-  //   e.stopPropagation();
-  //   e.preventDefault();
-  // }
-
-  // function dragover(e) {
-  //   e.stopPropagation();
-  //   e.preventDefault();
-  // }
-
-  // function drop(e) {
-  //   e.stopPropagation();
-  //   e.preventDefault();
-
-  //   var dt = e.dataTransfer;
-  //   var files = dt.files;
-
-  //   uploadFile(files);
-  // }
-  // dropZoneElement.addEventListener('dragenter', dragenter, false);
-  // dropZoneElement.addEventListener('dragover', dragover, false);
-  // dropZoneElement.addEventListener('drop', drop, false);
-
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
 
   var fileChooser = document.querySelector('.ad-form__field input[type=file]');
   var preview = document.querySelector('.ad-form-header__preview').querySelector('img');
   var dropZoneElement = document.querySelector('.ad-form-header__drop-zone');
-
 
   var uploadFile = function (file) {
 
@@ -93,8 +37,7 @@
 
   fileChooser.addEventListener('change', function () {
     uploadFile(fileChooser.files[0]);
-  }
-  );
+  });
 
   function dragenter(e) {
     e.stopPropagation();
@@ -110,13 +53,15 @@
     e.stopPropagation();
     e.preventDefault();
 
-    var dt = e.dataTransfer;
-    var file = dt.files;
-
-    uploadFile(file);
+    // var dt = e.dataTransfer;
+    // var files = dt.files;
+    var files = e.dataTransfer.getData('id', e.currentTarget.id);
+    uploadFile(files);
   }
-  
+
   dropZoneElement.addEventListener('dragenter', dragenter, false);
   dropZoneElement.addEventListener('dragover', dragover, false);
   dropZoneElement.addEventListener('drop', drop, false);
+
+
 })();
